8848370587374a9caaea9dc9c2fac33562080bae
Truc Tran <123161134+KieuTrucTran@users.noreply.github.com>
2024-04-17 23:45:48 +0200

    put my code in github

---
+++ main.js
@@ -0,0 +1,50 @@
+"use strict";
+const port = 3000,
+http = require("http"),
+httpStatus = require("http-status-codes"),
+router = require("./router"),
+contentTypes = require("./contentTypes"),
+utils = require("./utils");
+
+router.get("/", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.html);
+  utils.getFile("views/index.html", res);
+});
+router.get("/courses.html", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.html);
+  utils.getFile("views/courses.html", res);
+});
+router.get("/contact.html", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.html);
+  utils.getFile("views/contact.html", res);
+});
+router.post("/", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.html);
+  utils.getFile("views/thanks.html", res);
+});
+router.get("/contact.jpg", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.jpg);
+  utils.getFile("public/images/contact.jpg", res);
+});
+router.get("/pot.jpg", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.jpg);
+  utils.getFile("public/images/pot.jpg", res);
+});
+router.get("/product.jpg", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.jpg);
+  utils.getFile("public/images/product.jpg", res);
+});
+router.get("/cuisinique.css", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.css);
+  utils.getFile("public/css/cuisinique.css", res);
+});
+router.get("/bootstrap.css", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.css);
+  utils.getFile("public/css/bootstrap.css", res);
+});
+router.get("/cuisinique.js", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.js);
+  utils.getFile("public/js/cuisinique.js", res);
+});
+http.createServer(router.handle).listen(port);
+console.log(`The server is listening on port number: ${port}`);
